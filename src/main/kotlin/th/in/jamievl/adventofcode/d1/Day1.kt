c57c37951d99863d97460bfeca5e71e2dc028e58
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-12-03 21:49:36 -0700

    D1: Solve day 1 solution.

--- src/main/kotlin/th/in/jamievl/adventofcode/d1/Day1.kt
+++ src/main/kotlin/th/in/jamievl/adventofcode/d1/Day1.kt
@@ -1,5 +1,24 @@
 package th.`in`.jamievl.adventofcode.d1
 
+
 fun main() {
-    println("Hello from Kotlin!")
+    val inputFile = ClassLoader.getSystemResourceAsStream("d1/d1_input.txt")!!
+    val caloriesList = ArrayList<Int>()
+
+    with(inputFile.bufferedReader()) {
+        var line = this.readLine()
+        var currCounter = 0
+        while (line != null) {
+            if (line.isNotEmpty()) {
+                currCounter += line.toInt()
+            } else {
+                caloriesList.add(currCounter)
+                currCounter = 0
+            }
+
+            line = this.readLine()
+        }
+    }
+
+    println("max calories: ${caloriesList.max()}")
 }
