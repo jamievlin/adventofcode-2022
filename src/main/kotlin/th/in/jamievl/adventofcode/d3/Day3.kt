0699e00d00a2a7b9d4a54866d1e111171d44e658
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-12-12 00:04:34 -0700

    D3: Add day 3 solution for part 2.

--- src/main/kotlin/th/in/jamievl/adventofcode/d3/Day3.kt
+++ src/main/kotlin/th/in/jamievl/adventofcode/d3/Day3.kt
@@ -10,10 +10,21 @@ fun getElementPriority(charVal: Char) : Int {
     }
 }
 
+fun calculateGroupTagPriority(groupItemChar: List<Set<Char>>): Int {
+    val commonIntersectionGroup = groupItemChar.reduce { reducedSet, currSet ->
+        reducedSet intersect currSet
+    }
+    return getElementPriority(commonIntersectionGroup.elementAt(0))
+}
+
 fun main() {
     var totalPriority = 0
+    var groupTagCounter = 0
+    var groupTagPriority = 0
+    val commonItems =  ArrayList<Set<Char>>()
 
     readFromResource("d3/d3_input.txt") { line ->
+        // part 1
         val s1 = line.substring(0 until (line.length / 2))
         val s2 = line.substring(line.length/2)
 
@@ -22,7 +33,17 @@ fun main() {
 
         val commonChar = commonChars.elementAt(0)
         totalPriority += getElementPriority(commonChar)
+
+        // part 2
+        commonItems.add(line.toSet())
+        groupTagCounter = (groupTagCounter + 1) % 3
+
+        if (groupTagCounter == 0) {
+            groupTagPriority += calculateGroupTagPriority(commonItems)
+            commonItems.clear()
+        }
     }
 
-    print("total priority: $totalPriority")
+    println("total priority: $totalPriority")
+    println("group priority: $groupTagPriority")
 }
